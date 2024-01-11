c548275c79ae9a018f84cb91fa0b6a2dd94f24d5
snootfef <delingucanada@gmail.com>
2024-01-10 22:29:32 -0500

    Added dialogue history and ratings for each cat

--- src/App.js
+++ src/App.js
@@ -1,42 +1,64 @@
 import './App.css';
 import { useState } from "react";
 
-export default function App() {
-  const [cat, setCat] = useState(0);
+let catNames = ['Sniff', 'Mushroom', 'Fish', 'Onion', 'Chup', 'Mr. Whale', 'Gorp'];
+let cats = catNames.map((names) => ({ name: names, text: [], rating: 0 }));
+let clicked;
 
-  let name = ['Sniff', 'Mushroom', 'Fish', 'Onion', 'Chup', 'Mr. Whale', 'Gorp'];
+export default function App() {
+  const [currentCat, setCurrentCat] = useState(0);
+  const [likes, setLikes] = useState(0);
+  const [clicked, setClicked] = useState(false);
 
   function handleClick() {
-    let num = Math.floor(Math.random() * name.length);
-    while (num === cat) num = Math.floor(Math.random() * name.length);
-    setCat(num);
+    let num = Math.floor(Math.random() * cats.length);
+    while (num === currentCat) num = Math.floor(Math.random() * cats.length);
+    setClicked(false);
+    setCurrentCat(num);
   }
 
   return (
     <div className='page'>
       <div className='column1'>
-        <h1 className='h1'>{name[cat]}</h1>
+        <section className='interactionSection'>
+          <h1>{cats[currentCat].rating}</h1>
+          <h1>{cats[currentCat].name}</h1>
+        </section>
       </div>
       <div className='column2'>
         <button className='button' onClick={handleClick}>New Cat</button>
-        <Dialogue></Dialogue>
+        <Dialogue index={currentCat} setLikes={setLikes} clicked={clicked} setClicked={setClicked}></Dialogue>
       </div>
     </div>
   );
 }
 
-function Dialogue() {
+function Dialogue({ index, setLikes, clicked, setClicked }) {
+  if (cats[index].text.length === 0)
+    cats[index].text.push(generateText());
+
+  function handleClick(like) {
+    if (!clicked) {
+      if (like) cats[index].rating++;
+      else cats[index].rating--;
+      setClicked(true);
+      setLikes(cats[index].rating);
+    }
+  }
+
   return (
     <div>
-      <h2 className='h2'>
-        {generateText()}
-      </h2>
+      {cats[index].text.map((string) => <p>{string}</p>)}
+      <section className='section1'>
+        <button onClick={() => handleClick(true)}>Like</button>
+        <button onClick={() => handleClick(false)}>Dislike</button>
+      </section>
     </div>
   );
 }
 
 function generateText() {
-  let text = '', endText = Math.floor(Math.random() * 5), numWords = Math.floor(Math.random() * 5) + 1, puncNum = Math.floor(Math.random() * 4);
+  let text = '', endText = Math.floor(Math.random() * 7), numWords = Math.floor(Math.random() * 5) + 1, puncNum = Math.floor(Math.random() * 4);
   let punc = ['. ', '! ', '? ', '...']
 
   while (endText !== 3) {
@@ -46,7 +68,7 @@ function generateText() {
     text = text.substring(0, text.length - 1) + punc[puncNum];
     if (text.length > 85) break;
     numWords = Math.floor(Math.random() * 5) + 1;
-    endText = Math.floor(Math.random() * 5);
+    endText = Math.floor(Math.random() * 7);
     puncNum = Math.floor(Math.random() * 4)
   }
   return text;
